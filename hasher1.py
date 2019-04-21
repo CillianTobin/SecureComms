6c9c6ac963301d28c5acac9d92db11ef16a35e04
CillianTobin <32968296+CillianTobin@users.noreply.github.com>
2019-04-21 20:44:04 +0100

    Update hasher1.py

--- hasher1.py
+++ hasher1.py
@@ -1,5 +1,6 @@
 import hashlib
 
+# swaps the cases
 some_string = "ECSC"
 digits = list(some_string)
 for i in range(len(digits)):
@@ -8,6 +9,7 @@ new_string = ''.join(digits)
 
 target = 'c89aa2ffb9edcc6604005196b5f0e0e4'
 
+# generates hashes until target - last output is answer
 result = new_string
 while result != target:
     hash1 = result
